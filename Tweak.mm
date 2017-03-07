e64deaa944e9adff210171fe4df5cd8b4b278089
Abdullah Gabous <slogutis_dp@mohmal.com>
2017-03-07 20:58:17 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -9,3 +9,8 @@
   return YES;
   }
   %end
+
+    
+    // All My Tweaks + Programs under GPL licesen // 
+    // This Tweak should work on iOS 10 now //
+    // Any problem open issue and i will check //
